91b545f06f7bf97e488c12e67b7bbfc66a8700b3
jseveyka <jseveyka@gmail.com>
2026-07-07 20:06:55 -0700

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,478 @@
+window.ANIMATIONS = [
+  {
+    "title": "Torque Around A Joint",
+    "category": "Anatomy_Musculoskeletal",
+    "categoryLabel": "Anatomy Musculoskeletal",
+    "file": "torque-around-a-joint.html",
+    "url": "animations/Anatomy_Musculoskeletal/torque-around-a-joint.html"
+  },
+  {
+    "title": "Cranial Nerves Quiz",
+    "category": "Anatomy_Nervous_System",
+    "categoryLabel": "Anatomy Nervous System",
+    "file": "cranial-nerves-quiz.html",
+    "url": "animations/Anatomy_Nervous_System/cranial-nerves-quiz.html"
+  },
+  {
+    "title": "Biomolecule Quiz",
+    "category": "Cell_Biology_Chemistry",
+    "categoryLabel": "Cell Biology Chemistry",
+    "file": "biomolecule_quiz.html",
+    "url": "animations/Cell_Biology_Chemistry/biomolecule_quiz.html"
+  },
+  {
+    "title": "Endosymbiosis",
+    "category": "Cell_Biology_Chemistry",
+    "categoryLabel": "Cell Biology Chemistry",
+    "file": "endosymbiosis.html",
+    "url": "animations/Cell_Biology_Chemistry/endosymbiosis.html"
+  },
+  {
+    "title": "pH Buffers Animation",
+    "category": "Cell_Biology_Chemistry",
+    "categoryLabel": "Cell Biology Chemistry",
+    "file": "ph-buffers-animation.html",
+    "url": "animations/Cell_Biology_Chemistry/ph-buffers-animation.html"
+  },
+  {
+    "title": "Cell Signaling Pathways",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "cell-signaling-pathways.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/cell-signaling-pathways.html"
+  },
+  {
+    "title": "Cell To Cell Communication Systems",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "cell-to-cell-communication-systems.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/cell-to-cell-communication-systems.html"
+  },
+  {
+    "title": "Graded Potentials Single File",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "graded-potentials-single-file.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/graded-potentials-single-file.html"
+  },
+  {
+    "title": "Membrane Potential",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "membrane_potential.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/membrane_potential.html"
+  },
+  {
+    "title": "Neuronsummation",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "NeuronSummation.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/NeuronSummation.html"
+  },
+  {
+    "title": "Neurotransmitter Removal",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "Neurotransmitter_Removal.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/Neurotransmitter_Removal.html"
+  },
+  {
+    "title": "Resting Membrane Potential",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "Resting_membrane_potential.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/Resting_membrane_potential.html"
+  },
+  {
+    "title": "Synapses And Drugs",
+    "category": "Cell_signals_Membrane_Potentials_Neurons",
+    "categoryLabel": "Cell signals Membrane Potentials Neurons",
+    "file": "Synapses_and_Drugs.html",
+    "url": "animations/Cell_signals_Membrane_Potentials_Neurons/Synapses_and_Drugs.html"
+  },
+  {
+    "title": "Adiabatic Lapse Rates",
+    "category": "Climate_Ecology",
+    "categoryLabel": "Climate Ecology",
+    "file": "adiabatic_lapse_rates.html",
+    "url": "animations/Climate_Ecology/adiabatic_lapse_rates.html"
+  },
+  {
+    "title": "Coriolis Effect Global Winds",
+    "category": "Climate_Ecology",
+    "categoryLabel": "Climate Ecology",
+    "file": "coriolis-effect-global-winds.html",
+    "url": "animations/Climate_Ecology/coriolis-effect-global-winds.html"
+  },
+  {
+    "title": "Ocean Acidification Animation",
+    "category": "Climate_Ecology",
+    "categoryLabel": "Climate Ecology",
+    "file": "ocean-acidification-animation.html",
+    "url": "animations/Climate_Ecology/ocean-acidification-animation.html"
+  },
+  {
+    "title": "Tides Animation",
+    "category": "Climate_Ecology",
+    "categoryLabel": "Climate Ecology",
+    "file": "tides-animation.html",
+    "url": "animations/Climate_Ecology/tides-animation.html"
+  },
+  {
+    "title": "Basic Epithelial Tissue Types",
+    "category": "Histology",
+    "categoryLabel": "Histology",
+    "file": "basic-epithelial-tissue-types.html",
+    "url": "animations/Histology/basic-epithelial-tissue-types.html"
+  },
+  {
+    "title": "Tissue Changes Animation",
+    "category": "Histology",
+    "categoryLabel": "Histology",
+    "file": "tissue-changes-animation.html",
+    "url": "animations/Histology/tissue-changes-animation.html"
+  },
+  {
+    "title": "Tissue Dichotomous Key",
+    "category": "Histology",
+    "categoryLabel": "Histology",
+    "file": "Tissue-dichotomous-key.html",
+    "url": "animations/Histology/Tissue-dichotomous-key.html"
+  },
+  {
+    "title": "Antibody Functions Animation",
+    "category": "Immunity",
+    "categoryLabel": "Immunity",
+    "file": "antibody-functions-animation.html",
+    "url": "animations/Immunity/antibody-functions-animation.html"
+  },
+  {
+    "title": "Immune Receptor Diversity",
+    "category": "Immunity",
+    "categoryLabel": "Immunity",
+    "file": "immune-receptor-diversity.html",
+    "url": "animations/Immunity/immune-receptor-diversity.html"
+  },
+  {
+    "title": "Interferon Animation",
+    "category": "Immunity",
+    "categoryLabel": "Immunity",
+    "file": "interferon-animation.html",
+    "url": "animations/Immunity/interferon-animation.html"
+  },
+  {
+    "title": "MHC I Vs MHC Ii Animation",
+    "category": "Immunity",
+    "categoryLabel": "Immunity",
+    "file": "mhc-i-vs-mhc-ii-animation.html",
+    "url": "animations/Immunity/mhc-i-vs-mhc-ii-animation.html"
+  },
+  {
+    "title": "Tapeworm Life Cycle",
+    "category": "Life_cycles",
+    "categoryLabel": "Life cycles",
+    "file": "tapeworm-life-cycle.html",
+    "url": "animations/Life_cycles/tapeworm-life-cycle.html"
+  },
+  {
+    "title": "Ulva Life Cycle",
+    "category": "Life_cycles",
+    "categoryLabel": "Life cycles",
+    "file": "ulva-life-cycle.html",
+    "url": "animations/Life_cycles/ulva-life-cycle.html"
+  },
+  {
+    "title": "Viral Life Cycle",
+    "category": "Life_cycles",
+    "categoryLabel": "Life cycles",
+    "file": "viral-life-cycle.html",
+    "url": "animations/Life_cycles/viral-life-cycle.html"
+  },
+  {
+    "title": "Bacterial Exponential Growth",
+    "category": "Microbiology_Bacteria",
+    "categoryLabel": "Microbiology Bacteria",
+    "file": "bacterial_exponential_growth.html",
+    "url": "animations/Microbiology_Bacteria/bacterial_exponential_growth.html"
+  },
+  {
+    "title": "Bacterial Genera General Characteristics Quiz",
+    "category": "Microbiology_Bacteria",
+    "categoryLabel": "Microbiology Bacteria",
+    "file": "bacterial_genera_general_characteristics_quiz.html",
+    "url": "animations/Microbiology_Bacteria/bacterial_genera_general_characteristics_quiz.html"
+  },
+  {
+    "title": "Serial Dilution Simulation",
+    "category": "Microbiology_Bacteria",
+    "categoryLabel": "Microbiology Bacteria",
+    "file": "serial-dilution-simulation.html",
+    "url": "animations/Microbiology_Bacteria/serial-dilution-simulation.html"
+  },
+  {
+    "title": "Urease",
+    "category": "Microbiology_Bacteria",
+    "categoryLabel": "Microbiology Bacteria",
+    "file": "Urease.html",
+    "url": "animations/Microbiology_Bacteria/Urease.html"
+  },
+  {
+    "title": "ABC E Floral Pattern Genes",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "abc-e-floral-pattern-genes.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/abc-e-floral-pattern-genes.html"
+  },
+  {
+    "title": "Endosymbiosis",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "endosymbiosis.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/endosymbiosis.html"
+  },
+  {
+    "title": "Gene Duplication Animation",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "gene_duplication_animation.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/gene_duplication_animation.html"
+  },
+  {
+    "title": "Gene Expression Control",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "gene-expression-control.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/gene-expression-control.html"
+  },
+  {
+    "title": "Histone Modification DNA Compression",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "histone-modification-dna-compression.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/histone-modification-dna-compression.html"
+  },
+  {
+    "title": "Horizontal Gene Transfer Animation",
+    "category": "Molecular_Genetics_Genomic_Diversity",
+    "categoryLabel": "Molecular Genetics Genomic Diversity",
+    "file": "horizontal_gene_transfer_animation.html",
+    "url": "animations/Molecular_Genetics_Genomic_Diversity/horizontal_gene_transfer_animation.html"
+  },
+  {
+    "title": "Blood Transfusion",
+    "category": "Physiology_Blood",
+    "categoryLabel": "Physiology Blood",
+    "file": "Blood_transfusion.html",
+    "url": "animations/Physiology_Blood/Blood_transfusion.html"
+  },
+  {
+    "title": "Blood Typing",
+    "category": "Physiology_Blood",
+    "categoryLabel": "Physiology Blood",
+    "file": "blood_typing.html",
+    "url": "animations/Physiology_Blood/blood_typing.html"
+  },
+  {
+    "title": "Baroreceptor Chemoreceptor Reflexes",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "baroreceptor_chemoreceptor_reflexes.html",
+    "url": "animations/Physiology_Cardiovascular/baroreceptor_chemoreceptor_reflexes.html"
+  },
+  {
+    "title": "Blood Pressure Control",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "blood-pressure-control.html",
+    "url": "animations/Physiology_Cardiovascular/blood-pressure-control.html"
+  },
+  {
+    "title": "Blood Pressure Measurement",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "blood-pressure-measurement.html",
+    "url": "animations/Physiology_Cardiovascular/blood-pressure-measurement.html"
+  },
+  {
+    "title": "Bloodflow",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "BloodFLow.html",
+    "url": "animations/Physiology_Cardiovascular/BloodFLow.html"
+  },
+  {
+    "title": "Einthovens Triangle Animation",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "einthovens_triangle_animation.html",
+    "url": "animations/Physiology_Cardiovascular/einthovens_triangle_animation.html"
+  },
+  {
+    "title": "Heart Cells",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "Heart_cells.html",
+    "url": "animations/Physiology_Cardiovascular/Heart_cells.html"
+  },
+  {
+    "title": "Local Blood Flow Control",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "local-blood-flow-control.html",
+    "url": "animations/Physiology_Cardiovascular/local-blood-flow-control.html"
+  },
+  {
+    "title": "Movement Across Capillaries",
+    "category": "Physiology_Cardiovascular",
+    "categoryLabel": "Physiology Cardiovascular",
+    "file": "movement-across-capillaries.html",
+    "url": "animations/Physiology_Cardiovascular/movement-across-capillaries.html"
+  },
+  {
+    "title": "CYP450 Animation",
+    "category": "Physiology_Digestive_Urinary",
+    "categoryLabel": "Physiology Digestive Urinary",
+    "file": "cyp450-animation.html",
+    "url": "animations/Physiology_Digestive_Urinary/cyp450-animation.html"
+  },
+  {
+    "title": "Glomerulus Animation",
+    "category": "Physiology_Digestive_Urinary",
+    "categoryLabel": "Physiology Digestive Urinary",
+    "file": "Glomerulus-animation.html",
+    "url": "animations/Physiology_Digestive_Urinary/Glomerulus-animation.html"
+  },
+  {
+    "title": "Loop Of Henle Animation",
+    "category": "Physiology_Digestive_Urinary",
+    "categoryLabel": "Physiology Digestive Urinary",
+    "file": "loop-of-henle-animation.html",
+    "url": "animations/Physiology_Digestive_Urinary/loop-of-henle-animation.html"
+  },
+  {
+    "title": "Nutrient Absorption Animation",
+    "category": "Physiology_Digestive_Urinary",
+    "categoryLabel": "Physiology Digestive Urinary",
+    "file": "nutrient_absorption_animation.html",
+    "url": "animations/Physiology_Digestive_Urinary/nutrient_absorption_animation.html"
+  },
+  {
+    "title": "Hormone Quiz",
+    "category": "Physiology_Endocrine",
+    "categoryLabel": "Physiology Endocrine",
+    "file": "hormone_quiz.html",
+    "url": "animations/Physiology_Endocrine/hormone_quiz.html"
+  },
+  {
+    "title": "Tanning Feedback Animation",
+    "category": "Physiology_Integument",
+    "categoryLabel": "Physiology Integument",
+    "file": "tanning-feedback-animation.html",
+    "url": "animations/Physiology_Integument/tanning-feedback-animation.html"
+  },
+  {
+    "title": "Autonomic Nervous System Animation",
+    "category": "Physiology_Nervous_System_Autonomic",
+    "categoryLabel": "Physiology Nervous System Autonomic",
+    "file": "autonomic-nervous-system-animation.html",
+    "url": "animations/Physiology_Nervous_System_Autonomic/autonomic-nervous-system-animation.html"
+  },
+  {
+    "title": "Autonomic Quiz",
+    "category": "Physiology_Nervous_System_Autonomic",
+    "categoryLabel": "Physiology Nervous System Autonomic",
+    "file": "autonomic-quiz.html",
+    "url": "animations/Physiology_Nervous_System_Autonomic/autonomic-quiz.html"
+  },
+  {
+    "title": "Counter Current Heat Exchange",
+    "category": "Physiology_Reproductive",
+    "categoryLabel": "Physiology Reproductive",
+    "file": "counter-current-heat-exchange.html",
+    "url": "animations/Physiology_Reproductive/counter-current-heat-exchange.html"
+  },
+  {
+    "title": "HPO Ovarian Cycle Animation",
+    "category": "Physiology_Reproductive",
+    "categoryLabel": "Physiology Reproductive",
+    "file": "hpo-ovarian-cycle-animation.html",
+    "url": "animations/Physiology_Reproductive/hpo-ovarian-cycle-animation.html"
+  },
+  {
+    "title": "Spermatogenesis HPT Axis",
+    "category": "Physiology_Reproductive",
+    "categoryLabel": "Physiology Reproductive",
+    "file": "spermatogenesis-hpt-axis.html",
+    "url": "animations/Physiology_Reproductive/spermatogenesis-hpt-axis.html"
+  },
+  {
+    "title": "Carbon Dioxide Transport",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "carbon-dioxide-transport.html",
+    "url": "animations/Physiology_Respiratory/carbon-dioxide-transport.html"
+  },
+  {
+    "title": "Oxygen Hemoglobin Curve",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "oxygen_hemoglobin_curve.html",
+    "url": "animations/Physiology_Respiratory/oxygen_hemoglobin_curve.html"
+  },
+  {
+    "title": "Partial Pressures Gas Solubility",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "partial-pressures-gas-solubility.html",
+    "url": "animations/Physiology_Respiratory/partial-pressures-gas-solubility.html"
+  },
+  {
+    "title": "Spirometry Animation",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "spirometry-animation.html",
+    "url": "animations/Physiology_Respiratory/spirometry-animation.html"
+  },
+  {
+    "title": "Thoracic Pleura Animation",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "thoracic_pleura_animation.html",
+    "url": "animations/Physiology_Respiratory/thoracic_pleura_animation.html"
+  },
+  {
+    "title": "V/Q Ratio",
+    "category": "Physiology_Respiratory",
+    "categoryLabel": "Physiology Respiratory",
+    "file": "vq-ratio.html",
+    "url": "animations/Physiology_Respiratory/vq-ratio.html"
+  },
+  {
+    "title": "Bone Cell Control",
+    "category": "Physiology_Skeletal",
+    "categoryLabel": "Physiology Skeletal",
+    "file": "Bone_cell_control.html",
+    "url": "animations/Physiology_Skeletal/Bone_cell_control.html"
+  },
+  {
+    "title": "Cell Shape Surface Area Animation",
+    "category": "Scaling",
+    "categoryLabel": "Scaling",
+    "file": "cell_shape_surface_area_animation.html",
+    "url": "animations/Scaling/cell_shape_surface_area_animation.html"
+  },
+  {
+    "title": "Cube Scaling Animation",
+    "category": "Scaling",
+    "categoryLabel": "Scaling",
+    "file": "cube-scaling-animation.html",
+    "url": "animations/Scaling/cube-scaling-animation.html"
+  },
+  {
+    "title": "Limb Scaling And Posture",
+    "category": "Scaling",
+    "categoryLabel": "Scaling",
+    "file": "limb_scaling_and_posture.html",
+    "url": "animations/Scaling/limb_scaling_and_posture.html"
+  }
+];
